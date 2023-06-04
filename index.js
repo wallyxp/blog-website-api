c8c0ad3bd028643c785af6fbaa853d8f5a11b77a
Witty Wayne <debjyotinandi76@gmail.com>
2023-06-04 13:08:42 +0530

    implemented register user functionality

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
+const authRoute = require("./routes/auth")
 
 dotenv.config()
+app.use(express.json())
+
 mongoose.connect(process.env.MONGO_URL)
 .then(
     ()=>{
@@ -12,6 +15,7 @@ mongoose.connect(process.env.MONGO_URL)
 )
 .catch(err=>console.log(err))
 
+app.use("/api/auth", authRoute)
 
 app.listen("5000", ()=>{
     console.log("Server started on port 5000...");
