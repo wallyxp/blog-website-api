b965c2ac470517c127934e00588613ed4eec1cf1
Witty Wayne <debjyotinandi76@gmail.com>
2023-06-04 14:35:35 +0530

    implemented user authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,18 @@
 const router = require("express").Router()
 const User = require("../models/User")
+const bcrypt = require('bcrypt');
 
 // REGISTER
 //using post method
 
 router.post("/register", async (req, res)=>{
     try{
+        const salt = await bcrypt.genSalt(10)
+        const hashedPass = await bcrypt.hash(req.body.password, salt)
         const newUser = new User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPass,
         })
 
         const user = await newUser.save();
@@ -21,6 +24,29 @@ router.post("/register", async (req, res)=>{
 })
 
 // LOGIN
+
+router.post("/login", async (req, res) =>{
+    try{
+        const user = await User.findOne({
+            username: req.body.username
+        })
+        if(!user){
+            res.status(400).json("Wrong username or password")
+            return;
+        }
+
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        if(!validated){
+            res.status(400).json("Wrong username or password")
+            return;
+        }
+        const {password, ...other} = user._doc
+        res.status(200).json(other)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
 
 
 
